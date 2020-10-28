9746d6f1efc300876d94f970c7ebc6c800a49193
Edwin Moquete <edwinmmoquete@gmail.com>
2020-10-28 18:59:24 -0400

    sdm660-common: vibrator: Add excluded-input-devices.xml
    Signed-off-by: Aaradhay Vijay <inferno964.github@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -404,6 +404,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
